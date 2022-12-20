6f01c527c3fd187d999a23b67c2da06580c98b86
DominykMcNamara <dominyksmith@gmail.com>
2022-12-20 15:52:41 -0500

    Map through post data

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,18 +1,26 @@
-import React, { useState, useEffect} from 'react'
-import { useDispatch } from 'react-redux'
-import { useGetPostsQuery } from '../../app/redditApi'
-
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useGetPostsQuery } from "../../app/redditApi";
 
 export const PostList = () => {
-    const dispatch = useDispatch()
-    const { data, error, isLoading } = useGetPostsQuery()
+  const dispatch = useDispatch();
+  const { data, error, isLoading } = useGetPostsQuery();
 
-   
-        
-    
-    return (
-        <div>
-            Hello world
-        </div>
-    )
-}
+  useEffect(() => console.log(data));
+
+  let content;
+  if (isLoading) {
+    content = <h1>Loading....</h1>;
+  } else if (data) {
+    content = data.data.children.map((post) => (
+      <ul>
+        {" "}
+        <li key={post.data.id}>{post.data.id}</li>
+      </ul>
+    ));
+  } else {
+    content = <h1>{error}</h1>;
+  }
+
+  return <section>{content}</section>;
+};
