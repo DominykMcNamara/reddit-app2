811aa5a8bec0094ff33a2e8d2dab7da7a84aecf9
DominykMcNamara <dominyksmith@gmail.com>
2022-12-22 23:24:14 -0500

    fix bg

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import RedditLogo from "../assets/reddit-4.svg";
 import { Searchbar } from "./SearchBar";
 export const Header = () => {
   return (
-    <div className="container-fluid py-5 h-25 bg-secondary ">
+    <div className="container-fluid py-5 h-25">
       <div className="d-flex flex-row justify-content-around w-75 mx-auto">
         <img
           id="logo"
